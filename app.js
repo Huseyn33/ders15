714a80951a50ffa20e34652e8085e8f2a6e2ab2b
Huseyn33 <huseynoyunkanali1@gmail.com>
2025-04-07 09:31:30 +0400

    finish

---
+++ app.js
@@ -0,0 +1,15 @@
+let ad="huseyn"
+console.log(typeof ad)
+let yas=12
+console.log(typeof yas)
+let sevdiyinreng="qirmizi"
+console.log(typeof sevdiyinreng)
+let bugundersinioxumusan=15>6
+let yes= false
+let no=true
+console.log(typeof bugundersinioxumusan)
+var a;
+var b=null;
+console.log(typeof a)
+console.log(typeof b)
+console.log(`salam senin adin ${ad} ve senin yasin ${yas} dir senin sevdiyin ren ise ${sevdiyinreng} dir sen bugun dersini ${bugundersinioxumusan}`)
